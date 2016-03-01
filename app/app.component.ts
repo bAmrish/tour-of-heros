fd074fbc6cbd285213307169e0f69351344c82d8
Amrish Bharatiya <bamrish@gmail.com>
2016-02-29 20:16:37 -0500

    Step 2 of the tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,24 +5,97 @@ interface Hero {
     name: string;
 }
 
+var HEROS: Hero[] = [
+    {id: 1, name: 'Mogambo'},
+    {id: 2, name: 'Shakal'},
+    {id: 3, name: 'Gabbar'},
+    {id: 4, name: 'Lion'},
+    {id: 5, name: 'Ravan'},
+    {id: 6, name: 'Kans'},
+    {id: 7, name: 'Gogo'},
+    {id: 8, name: 'Teja'},
+    {id: 9, name: 'Robert'},
+    {id: 10, name: 'Bhalla'}
+]
+
 @Component ({
     selector: 'my-app',
     template: `
         <h1>{{title}}</h1>
-        <h2>{{hero.name}} details!!</h2>
-        <div> <label for="id">id: </label> {{hero.id}}</div>
-        <div>
-            <label for="name">name</label>
+        <ul class="heroes">
+            <li *ngFor="#hero of heros" (click)="onSelect(hero)"
+                [class.selected]="selectedHero===hero">
+                <span class="badge">{{hero.id}}</span>
+                {{hero.name}}
+            </li>
+        </ul>
+        <div *ngIf="selectedHero">        
+            <h2>{{selectedHero.name}} details!!</h2>
+            <div> <label for="id">id: </label> {{selectedHero.id}}</div>
             <div>
-                <input type="text" [(ngModel)]=hero.name placehodler="name"/>
+                <label for="name">name</label>
+                <div>
+                    <input type="text" [(ngModel)]=selectedHero.name placehodler="name"/>
+                </div>
             </div>
         </div>
-    `
+    `,
+    styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 10em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
 export class AppComponent {
-    public title = 'Tour of Heros'
-    public hero: Hero = {
-        id: 1,
-        name: 'Windstrom'
+    public title = 'Tour of Heros';
+    public selectedHero: Hero;
+    public heros = HEROS;
+
+    onSelect(hero: Hero) {
+        this.selectedHero = hero;
     }
 }
